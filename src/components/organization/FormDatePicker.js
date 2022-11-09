4b7dd7b3b36726263e2f09bd883b6745fe675195
Sachintha Gunaratne <sachinthasl99@gmail.com>
2022-11-09 13:35:01 +0530

    edit fund

--- src/components/organization/FormDatePicker.js
+++ src/components/organization/FormDatePicker.js
@@ -2,29 +2,12 @@ import React, { useState } from 'react'
 import { Pressable, View } from 'react-native'
 import { TextInput } from 'react-native-paper'
 import DateTimePicker from '@react-native-community/datetimepicker';
+import { convertDateToString } from '../../utils/converDateToString';
 
-function FormDatePicker({ onChangeDate, minDate, maxDate }) {
+function FormDatePicker({ onChangeDate, minDate, maxDate, value }) {
     const [date, setDate] = useState(new Date());
-    const [dateString, setDateString] = useState("");
+    const [dateString, setDateString] = useState(value ? convertDateToString(value) : "");
     const [show, setShow] = useState(false);
-
-    // convert date to string
-    const convertDateToString = (date) => {
-        let year = date.getFullYear();
-        let month = date.getMonth() + 1;
-        let day = date.getDate();
-
-        // add 0 to month and day if they are less than 10
-        if (month < 10) {
-            month = '0' + month;
-        }
-        if (day < 10) {
-            day = '0' + day;
-        }
-        const dateString = `${year}-${month}-${day}`;
-
-        return dateString;
-    }
 
     const onChange = (event, selectedDate) => {
         onChangeDate(selectedDate); // pass date to parent component
